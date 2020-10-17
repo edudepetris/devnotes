2bf7a82f5eb0159e653dfd04f7b49341c763a2a1
Edu Depetris <edu.depetris.00@gmail.com>
2020-10-17 18:12:39 +1300

    Skip test file on production precompilation

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+// https://github.com/rails/webpacker/issues/1818#issuecomment-533589724
+environment.loaders.get('babel').exclude = [
+  /\.test\.js$/,
+  /\.stories\.js$/,
+  environment.loaders.get('babel').exclude,
+]
+
 module.exports = environment.toWebpackConfig()
