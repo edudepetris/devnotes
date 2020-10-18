c005997934e05ba0eeeccdd2c7dcf63d47680ad3
Edu Depetris <edu.depetris.00@gmail.com>
2020-10-18 19:37:34 +1300

    Update theme toggler

--- app/javascript/react/components/ThemeToggler/index.js
+++ app/javascript/react/components/ThemeToggler/index.js
@@ -4,7 +4,7 @@ import {useColorMode, Box, IconButton} from '@chakra-ui/core'
 export default function ThemeToggler() {
   const {colorMode, toggleColorMode} = useColorMode()
   return (
-    <Box textAlign="right" py={4} mr={12}>
+    <Box textAlign="right">
       <IconButton
         icon={colorMode === 'light' ? 'moon' : 'sun'}
         onClick={toggleColorMode}
