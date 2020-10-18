be077fd1d8fcef1fd4a057702b3b66e763f5b0d5
Edu Depetris <edu.depetris.00@gmail.com>
2020-10-18 19:39:29 +1300

    Add dashboard structure

---
+++ app/javascript/react/components/NavBar/index.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import {
+  Icon,
+  Box,
+  Heading,
+  Flex,
+  Text,
+  Button,
+  MenuItem,
+  MenuGroup,
+  MenuButton,
+  Menu,
+  MenuDivider,
+  MenuList,
+} from '@chakra-ui/core'
+import ThemeToggler from '../ThemeToggler'
+
+const MenuItems = ({children}) => (
+  <Text mt={{base: 4, md: 0}} mr={6} display="block">
+    {children}
+  </Text>
+)
+
+const NavBar = (props) => {
+  const [show, setShow] = React.useState(false)
+  const handleToggle = () => setShow(!show)
+
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1.5rem"
+      bg="transparent"
+      color="currentColor"
+    >
+      <Flex align="center" mr={5}>
+        <Heading as="h1" size="lg">
+          Devnotes
+        </Heading>
+      </Flex>
+
+      <Box display={{sm: 'block', md: 'none'}} onClick={handleToggle}>
+        <Icon name="menu" />
+      </Box>
+
+      <Box
+        display={{base: show ? 'block' : 'none', md: 'flex'}}
+        width={{base: 'full', md: 'auto'}}
+        alignItems="center"
+        flexGrow={1}
+      >
+        <MenuItems>Docs</MenuItems>
+        <MenuItems>Examples</MenuItems>
+        <MenuItems>Blog</MenuItems>
+        <ThemeToggler />
+      </Box>
+
+      <Box
+        display={{base: show ? 'block' : 'none', md: 'block'}}
+        mt={{base: 4, md: 0}}
+      >
+        <Menu>
+          <MenuButton as={Button}>Profile</MenuButton>
+          <MenuList>
+            <MenuGroup title="Profile">
+              <MenuItem>My Account</MenuItem>
+              <MenuItem>Payments </MenuItem>
+            </MenuGroup>
+            <MenuDivider />
+            <MenuGroup title="Help">
+              <MenuItem>Docs</MenuItem>
+              <MenuItem>FAQ</MenuItem>
+            </MenuGroup>
+          </MenuList>
+        </Menu>
+      </Box>
+    </Flex>
+  )
+}
+
+export default NavBar
--- app/javascript/react/theme.js
+++ app/javascript/react/theme.js
@@ -2,8 +2,24 @@
 import React from 'react' // eslint-disable-line no-unused-vars
 import {theme} from '@chakra-ui/core'
 
+const navBarIcons = {
+  menu: {
+    path: (
+      <path
+        fill="currentColor"
+        d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"
+      />
+    ),
+    viewBox: '0 0 20 20',
+  },
+}
+
 const devnotesTheme = {
   ...theme,
+  icons: {
+    ...theme.icons,
+    ...navBarIcons,
+  },
 }
 
 export default devnotesTheme
