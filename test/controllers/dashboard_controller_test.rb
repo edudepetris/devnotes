72b53d82de89d4025cb3afa5dd7a39d770b6e2bc
Edu Depetris <edu.depetris.00@gmail.com>
2020-10-18 21:15:37 +1300

    Update test

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   test 'redirect when guest user' do
-    get dashboard_path
+    get dashboard_url
     assert_response 302
   end
 
   test 'render dashboard when logged in user' do
     sign_in users(:one)
-    get dashboard_path
+    get dashboard_url
     assert_response :success
   end
 end
