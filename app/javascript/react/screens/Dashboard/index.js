d94bf28841f9a0f2c9e05e76f3610f0408e4cd61
Edu Depetris <edu.depetris.00@gmail.com>
2020-10-24 13:39:53 +1300

    Ping frontend/notes

--- app/javascript/react/screens/Dashboard/index.js
+++ app/javascript/react/screens/Dashboard/index.js
@@ -7,16 +7,54 @@ import ThemeProvider from '../../components/ThemeProvider'
 import NavBar from '../../components/NavBar'
 import StackNotes from '../../components/StackNotes'
 
+import {getNoteById} from '../../api'
+
 const Dashboard = ({authenticityToken, notes}) => {
+  const [selectedNote, setSelectedNote] = React.useState(null)
+
+  const [loading, setLoading] = React.useState(true)
+  const [note, setNote] = React.useState(null)
+  const [error, setError] = React.useState(null)
+
+  React.useEffect(() => {
+    if (!selectedNote) {
+      return
+    }
+    setLoading(true)
+
+    getNoteById(selectedNote.id)
+      .then((res) => res.json())
+      .then((data) => {
+        setNote(data)
+        setError(null)
+        setLoading(false)
+      })
+      .catch((e) => {
+        console.warn(e.message)
+        setError('Error fetching data. Try again.')
+        setLoading(false)
+      })
+  }, [selectedNote])
+
+  const handleSelectedNote = (selected) => {
+    setSelectedNote(selected)
+  }
+
   return (
     <ThemeProvider>
       <NavBar authenticityToken={authenticityToken} />
 
       <Grid templateColumns="1fr 3fr" gap={4} overflow="hidden">
         <Box w="100%" h="100vh" p="2" overflowY="scroll">
-          <StackNotes notes={notes} selectedNoteId={3} />
+          <StackNotes notes={notes} handleSelectedNote={handleSelectedNote} />
         </Box>
-        <Box w="100%" h="100vh" p="2" overflowY="scroll" />
+        <Box w="100%" h="100vh" p="2" overflowY="scroll">
+          {!loading && error && <>Error</>}
+
+          {loading && <>Loading</>}
+
+          {!loading && note && JSON.stringify(note, undefined, 2)}
+        </Box>
       </Grid>
     </ThemeProvider>
   )
