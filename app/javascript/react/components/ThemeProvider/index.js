416c6e978c8cb640e7037c02bf0eadbaca573334
Edu Depetris <edu.depetris.00@gmail.com>
2021-02-27 17:17:36 +1300

    Update ThemeProvider with new component

--- app/javascript/react/components/ThemeProvider/index.js
+++ app/javascript/react/components/ThemeProvider/index.js
@@ -1,22 +1,13 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
-import {
-  ThemeProvider as ChakraThemeProvider,
-  CSSReset,
-  ColorModeProvider,
-} from '@chakra-ui/core'
+import {ChakraProvider} from '@chakra-ui/react'
 import devnotesTheme from '../../theme'
 
 // https://chakra-ui.com/getting-started
 // https://chakra-ui.com/color-mode
 // Our own ThemeProvider.
 const ThemeProvider = ({children}) => (
-  <ChakraThemeProvider theme={devnotesTheme}>
-    <ColorModeProvider>
-      <CSSReset />
-      {children}
-    </ColorModeProvider>
-  </ChakraThemeProvider>
+  <ChakraProvider theme={devnotesTheme}>{children}</ChakraProvider>
 )
 
 export default ThemeProvider
