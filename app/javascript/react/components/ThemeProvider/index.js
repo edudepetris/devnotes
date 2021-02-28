da8c951ebcce85d66e3149688a05720ada8583b9
Edu Depetris <edu.depetris.00@gmail.com>
2021-02-28 17:10:41 +1300

    Add color mode script to CustomTheme

--- app/javascript/react/components/ThemeProvider/index.js
+++ app/javascript/react/components/ThemeProvider/index.js
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
-import {ChakraProvider} from '@chakra-ui/react'
+import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
+
 import devnotesTheme from '../../theme'
 
 // https://chakra-ui.com/getting-started
 // https://chakra-ui.com/color-mode
 // Our own ThemeProvider.
 const ThemeProvider = ({children}) => (
-  <ChakraProvider theme={devnotesTheme}>{children}</ChakraProvider>
+  <>
+    <ColorModeScript initialColorMode={devnotesTheme.config.initialColorMode} />
+    <ChakraProvider theme={devnotesTheme}>{children}</ChakraProvider>
+  </>
 )
 
 export default ThemeProvider
