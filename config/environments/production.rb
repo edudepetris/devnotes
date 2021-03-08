d8af7e0c52d6f4c778d29f17311b599a482d896f
Edu Depetris <edu.depetris.00@gmail.com>
2021-03-08 22:02:00 +1300

    Add production email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,16 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'devnotes-production.herokuapp.com',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { protocol: 'https', host: 'devnotes-production.herokuapp.com' }
 end
