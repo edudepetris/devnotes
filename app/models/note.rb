d3cb73fbac63fd0104b1027a8443c405a0a3d679
Edu Depetris <edu.depetris.00@gmail.com>
2021-03-17 00:26:29 +1300

    Add scope by user on project_name
    Users can share project names.

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,5 +3,5 @@ class Note < ApplicationRecord
   belongs_to :user
 
   validates :content, :project_name, presence: true
-  validates :project_name, uniqueness: true
+  validates :project_name, uniqueness: { scope: :user }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_093516) do
+ActiveRecord::Schema.define(version: 2021_03_16_111747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(version: 2020_09_23_093516) do
 
   create_table "notes", force: :cascade do |t|
     t.text "content"
-    t.string "project_name"
+    t.string "project_name", null: false
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["project_name"], name: "index_notes_on_project_name", unique: true
+    t.index ["project_name", "user_id"], name: "index_notes_on_project_name_and_user_id", unique: true
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
